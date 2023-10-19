405c24efc4449628e56816b829028c514a18555b
TheColdGuy251 <smirnovilya7031@gmail.com>
2023-10-19 18:38:12 +0500

    Task2

---
+++ Tyuiu.SmirnovIA.Sprint3.Task2.V7.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.SmirnovIA.Sprint3.Task2.V7.Lib;
+
+namespace Tyuiu.SmirnovIA.Sprint3.Task2.V7.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            int startValue = 1;
+            int stopValue = 10;
+            var res = ds.GetSumSeries(startValue, stopValue);
+            Assert.AreEqual(81.083, res);
+        }
+    }
+}
