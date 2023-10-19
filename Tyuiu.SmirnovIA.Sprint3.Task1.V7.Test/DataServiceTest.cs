b9f076e7d134c98371847c26eab051c2517bb9b5
TheColdGuy251 <smirnovilya7031@gmail.com>
2023-10-19 18:18:25 +0500

    Task1

---
+++ Tyuiu.SmirnovIA.Sprint3.Task1.V7.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.SmirnovIA.Sprint3.Task1.V7.Lib;
+
+namespace Tyuiu.SmirnovIA.Sprint3.Task1.V7.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double value = 0.25;
+            int startValue = 1;
+            int stopValue = 9;
+            var res = ds.GetMultiplySeries(value, startValue, stopValue);
+            Assert.AreEqual(-0.93, res);
+        }
+    }
+}
---
+++ Tyuiu.SmirnovIA.Sprint3.Task1.V7/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.SmirnovIA.Sprint3.Task1.V7.Lib;
+
+namespace Tyuiu.SmirnovIA.Sprint3.Task1.V7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнил: Смирнов И. А. | ИИПб-23-3";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Оператор цикла while                                              *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #7                                                              *");
+            Console.WriteLine("* Выполнил: Смирнов Илья Алексеевич | ИИПб-23-3                           *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу используя цикл while, которая вычисляет произведение *");
+            Console.WriteLine("* ряда по формуле, при a = 0.25                                           *");
+            Console.WriteLine("*     9   k                                                               *");
+            Console.WriteLine("* p = П (a  + 1 )sin(5)                                                   *");
+            Console.WriteLine("*    k=1                                                                  *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            double value = 0.25;
+            int startValue = 1;
+            int stopValue = 9;
+
+            Console.WriteLine("Переменная a = " + value);
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Произведение ряда = " + ds.GetMultiplySeries(value, startValue, stopValue));
+
+            Console.ReadKey();
+        }
+    }
+}
