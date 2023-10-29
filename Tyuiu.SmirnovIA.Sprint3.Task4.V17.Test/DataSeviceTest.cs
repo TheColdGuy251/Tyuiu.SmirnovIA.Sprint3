aa330d0e6a5cab594570b0ea4782a47c88bf28e0
TheColdGuy251 <smirnovilya7031@gmail.com>
2023-10-29 16:57:51 +0500

    Task4

---
+++ Tyuiu.SmirnovIA.Sprint3.Task4.V17.Test/DataSeviceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.SmirnovIA.Sprint3.Task4.V17.Lib;
+
+namespace Tyuiu.SmirnovIA.Sprint3.Task4.V17.Test
+{
+    [TestClass]
+    public class DataSeviceTest
+    {
+        [TestMethod]
+        public void ValidCalc()
+        {
+            DataService ds = new DataService();
+            int startValue = -5;
+            int stopValue = 5;
+
+            double res = ds.Calculate(startValue, stopValue);
+            Assert.AreEqual(0.527, res);
+        }
+    }
+}
---
+++ Tyuiu.SmirnovIA.Sprint3.Task4.V17.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.SmirnovIA.Sprint3.Task4.V17.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.SmirnovIA.Sprint3.Task4.V17.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cef6e08a-443b-4033-b305-fc912ccd22f8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
